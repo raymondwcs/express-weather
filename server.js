1594db0788af76cb89dca1f11cdb439c20eab213
Raymond So <wcrso@MacBook-Retina-Grey.local>
2017-09-03 18:32:55 +0800

    fixed bugs

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ cors = require('cors');
 
 var express = require('express');
 var app = express();
-var APIKEY = "";  // signup at api.openweathermap.org and obtain an API Key
+var APIKEY = "";  //signup at api.openweathermap.org and obtain an API Key
 
 var options = {
     host: 'api.openweathermap.org',
