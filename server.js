62a45d6c1dafe8ceb042330cca651f338f48c26e
Raymond SO <rso@ouhk.edu.hk>
2019-05-09 18:16:45 +0800

    Used async/await

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-http = require('http');
-cors = require('cors');
+const http = require('http');
+const cors = require('cors');
 
-var express = require('express');
-var app = express();
-var APIKEY = "";  //signup at api.openweathermap.org and obtain an API Key
+const app = express();
+const express = require('express');
+const APIKEY = "";  //signup at api.openweathermap.org and obtain an API Key
 
 var options = {
     host: 'api.openweathermap.org',
@@ -25,20 +25,20 @@ app.get('/weather',function(req,res) {
 
 	console.log("City: " + city);
 	setOptionPath(city);
-	getWeatherDetails(function(data){ 
-			data.city = city.toUpperCase();
-			res.render('weather', data);
-	}); 
-}); // end of app.get()
+	getWeatherDetails().then(data => {
+    	data.city = city.toUpperCase();
+    	res.render('weather', data);
+   })
+});
 
 app.get('/api/weather',function(req,res) {
 	var city = req.query.city;
 
 	console.log("City: " + city);
 	setOptionPath(city);
-	getWeatherDetails(function(data) {
-		res.json(data);
-	})
+	getWeatherDetails().then(data => {
+    	res.end.json(data);
+   })
 })
 
 app.listen(process.env.PORT || 8099);
@@ -47,43 +47,24 @@ function setOptionPath(city) {
 	options.path = "/data/2.5/weather?q=" + city.replace(/ /g,"+") + "&units=metric&APPID=" + APIKEY;
 }
 
-function getWeatherDetails(callback) {
-	var currTemp = 'N/A';
-	var maxTemp = 'N/A';
-	var minTemp = 'N/A';
-	var humidity = 'N/A';
+async function getWeatherDetails() {
+	let currTemp = 'N/A';
+	let maxTemp = 'N/A';
+	let minTemp = 'N/A';
+	let letidity = 'N/A';
 
-	var wreq = http.request(options, function(wres,res) {
-   	wres.setEncoding('utf8');
-
-		wres.on('data', function (chunk) {
-			var jsonObj = JSON.parse(chunk);
-			if (!jsonObj.hasOwnProperty("main")) {
-				jsonObj.main = 'N/A';
-			}
-			console.log("Current Temp. : " + jsonObj.main.temp);
-			console.log("Max Temp : "      + jsonObj.main.temp_max);
-			console.log("Min Temp : "      + jsonObj.main.temp_min);
-			console.log("Humidity : "      + jsonObj.main.humidity);
-									
-			currTemp = jsonObj.main.temp;
-			maxTemp = jsonObj.main.temp_max;
-			minTemp = jsonObj.main.temp_min;
-			humidity = jsonObj.main.humidity;
-		});
-
-		wres.on('error',function(e) {
-			console.log('Problem with request: ' + e.message);
-		});
-
-		wres.on('end',function(chunk) {
-			callback({currTemp: currTemp,
-			          maxTemp: maxTemp,
-			          minTemp: minTemp,
-			          humidity: humidity});
-		});
-  }); //http.request
-	
-	wreq.end();
+   const res = await fetch(`http://${options.host}/${options.path}`);
+   const data = await res.json();
+   
+   currTemp = data.main.temp;
+   maxTemp = data.main.temp_max;
+   minTemp = data.main.temp_min;
+   humidity = data.main.humidity;
+   
+   return({
+      currTemp: currTemp,
+      maxTemp: maxTemp,
+      minTemp: minTemp,
+      humidity: humidity
+   });
 }
-
